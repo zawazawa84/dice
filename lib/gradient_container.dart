e7af89580c17b20b26d519a5f110b80106a5f783
aizawa <ikiatawazia@outlook.jp>
2023-05-18 02:26:45 -0700

    テキストスタイルのファイル分割

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:dice/styled_text.dart';
+
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
 
@@ -13,10 +15,7 @@ class GradientContainer extends StatelessWidget {
         ], begin: Alignment.topLeft, end: Alignment.bottomRight),
       ),
       child: const Center(
-        child: Text(
-          "Hello World!",
-          style: TextStyle(color: Colors.white, fontSize: 28),
-        ),
+        child: StyledText()
       ),
     );
   }
