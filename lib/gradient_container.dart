1da64d9791b42c415631fe96a6ca3c809e6b3a35
aizawa <ikiatawazia@outlook.jp>
2023-05-18 02:03:35 -0700

    keyの設定

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+
   @override
   Widget build(context) {
     return Container(
