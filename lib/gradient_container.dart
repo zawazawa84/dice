b80c6883c7fdcf58a785b81b5efb08d35161e3f7
aizawa <ikiatawazia@outlook.jp>
2023-05-18 02:48:09 -0700

    const

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 
 import 'package:dice/styled_text.dart';
 
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
+
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
 
@@ -12,11 +15,9 @@ class GradientContainer extends StatelessWidget {
         gradient: LinearGradient(colors: [
           Color.fromARGB(255, 26, 2, 80),
           Color.fromARGB(255, 45, 7, 98),
-        ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+        ], begin: startAlignment, end: endAlignment),
       ),
-      child: const Center(
-        child: StyledText()
-      ),
+      child: const Center(child: StyledText()),
     );
   }
 }
