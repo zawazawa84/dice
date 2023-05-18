4415ae0a769836cb978dc26d7ede343de5a5be27
aizawa <ikiatawazia@outlook.jp>
2023-05-18 00:56:44 -0700

    グラデーション

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,25 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: Scaffold(
-      body: Center(
-        child: Text("Hello World!"),
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 26, 2, 80),
+                Color.fromARGB(255, 45, 7, 98)
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight
+            )
+          ),
+          child: const Center(
+            child: Text("Hello World!"),
+          ),
+        ),
       ),
-    ),
-  ));
+    )
+  );
 }
