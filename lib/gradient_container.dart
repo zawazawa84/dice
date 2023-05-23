2a66d976070f5057f6ded419d37c493e286bfadd
aizawa <ikiatawazia@outlook.jp>
2023-05-23 13:00:18 -0700

    ダイスボタンの表示

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -13,20 +13,36 @@ class GradientContainer extends StatelessWidget {
   final Color color1;
   final Color color2;
 
+  void rollDice() {
+
+  }
+
   @override
   Widget build(context) {
     return Container(
       decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: [color1,color2], 
-          begin: startAlignment, 
-          end: endAlignment
-        ),
+            colors: [color1, color2], begin: startAlignment, end: endAlignment),
       ),
       child: Center(
-        child: Image.asset(
-          'assets/images/dice-2.png',
-          width: 200,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.asset(
+              'assets/images/dice-2.png',
+              width: 200,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            TextButton(
+              onPressed: rollDice,
+              style: TextButton.styleFrom(
+                foregroundColor: Colors.white,
+                textStyle: const TextStyle(fontSize: 28),
+            ),
+            child: const Text('Roll Dice'))
+          ],
         )
       ),
     );
