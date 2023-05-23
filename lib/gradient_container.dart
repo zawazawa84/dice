aeced6255347d144e1ca0adb02fe1cb7425fddaf
aizawa <ikiatawazia@outlook.jp>
2023-05-23 12:43:19 -0700

    gradient_containerにpropsを渡す

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -6,16 +6,24 @@ const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
+  const GradientContainer(this.color1, this.color2, {super.key});
+
+  const GradientContainer.purple({super.key})
+      : color1 = Colors.deepPurple,
+        color2 = Colors.indigo;
+
+  final Color color1;
+  final Color color2;
 
   @override
   Widget build(context) {
     return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(colors: [
-          Color.fromARGB(255, 26, 2, 80),
-          Color.fromARGB(255, 45, 7, 98),
-        ], begin: startAlignment, end: endAlignment),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [color1,color2], 
+          begin: startAlignment, 
+          end: endAlignment
+        ),
       ),
       child: const Center(child: StyledText('Hello World!')),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,9 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        body: GradientContainer(),
+        body: GradientContainer(
+          Color.fromARGB(255, 26, 2, 80),
+          Color.fromARGB(255, 45, 7, 98),),
       ),
     ),
   );
